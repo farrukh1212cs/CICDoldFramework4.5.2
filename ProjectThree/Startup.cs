cbc27cd8c15a0a8a16862d2e1070c61dd86c65d4
farrukh.rehman <partner.farrukh.rehman@axian-group.com>
2023-02-10 14:07:56 +0500

    Add project files.

---
+++ ProjectThree/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectThree.Startup))]
+namespace ProjectThree
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
